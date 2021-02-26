a0e2e2dfe74aadd259757e7c14ee7bca4ee29874
Dams2 <damien-rojo@hotmail.fr>
2021-02-26 10:19:50 +0100

    Initial Commit

---
+++ LoTerra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoTerra
+//
+//  Created by Damien Rojo on 26.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
